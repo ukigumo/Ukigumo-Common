2a65bb3fcdc76b79125834511853abb886c54fc4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-11 13:26:50 +0900

    modernize package

--- lib/Ukigumo/Common.pm
+++ lib/Ukigumo/Common.pm
@@ -27,7 +27,7 @@ This distribution should on perl 5.8.
 
 =head1 AUTHOR
 
-Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
 
 =head1 SEE ALSO
 
